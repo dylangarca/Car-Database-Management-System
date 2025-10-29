c52e33c5fd1d89fa2f9c8c3709a8ef94bb9a01f0
Dylan Garcia <dg599@njit.edu>
2025-10-29 08:46:22 -0400

    added helper functions and templating for navigation

--- partials/nav.php
+++ partials/nav.php
@@ -1,4 +1,8 @@
 <?php
+//include functions here so we can have it on every page that uses the nav bar
+//that way we don't need to include so many other files on each page
+//nav will pull in functions and functions will pull in db
+
 // checking to see if domain has a port number attached (localhost)
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
@@ -17,12 +21,19 @@ if ($domain != "localhost") {
     ]);
 }
 session_start();
-require(__DIR__."/../lib/functions.php"); 
+require(__DIR__."/../lib/functions.php");
 ?>
-<nav> 
+<nav>
     <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-        <li><a href="logout.php">Logout</a></li>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="home.php">Home</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
     </ul>
 </nav>
