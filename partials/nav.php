a6708938a1baee8406a637782023b7db792758ce
Dylan Garcia <dg599@njit.edu>
2025-11-03 09:40:42 -0500

    added username and profile functionality

--- partials/nav.php
+++ partials/nav.php
@@ -26,7 +26,8 @@ require(__DIR__."/../lib/functions.php");
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
-            <li><a href="home.php">Home</a></li>
+            <li><a href="landing.php">Landing</a></li>
+            <li><a href="profile.php">Profile</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
