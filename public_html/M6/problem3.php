75c755f562c81f48d5f2b20ee7e4edb945f10523
Dylan Garcia <dg599@njit.edu>
2025-11-03 17:13:23 -0500

    adding pdf

--- public_html/M6/problem3.php
+++ public_html/M6/problem3.php
@@ -53,9 +53,9 @@ function joinArrays($users, $activities) {
     //dg599 11/3
     //plan
     //loop through each user in the users array
-    // for each user find the matching activity in the activities array by the userid
+    //for each user find the matching activity in the activities array by the userid
     //combine the user data with the activity data into 1 array
-    // add the combined arry to joined array
+    //add the combined arry to joined array
 
     foreach($users as $user){
         foreach ($activities as $activity){
